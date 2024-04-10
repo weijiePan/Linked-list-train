e776b0f4d65856850aec73a95d7a8234c036c1be
Weijie Pan <lengendseerx@gmail.com>
2024-04-10 11:24:00 -0400

    added print train cart functionality

--- cart.py
+++ cart.py
@@ -1,9 +1,14 @@
-class node:
-    color = None
-    next_cart = None
+class cart:
+
     def __init__(self, color, next_cart = None):
                 self.color = color
                 if(next_cart != None):
                     self.next_cart = next_cart 
+                else:
+                    self.next_cart = None
+    
+    def __repr__(self):
+           
+           return f" ------\n|{self.color}|\n ------ "
    
 
